1d179422900d8f2078bfb91b6b9c715a5af2a65b
NotVenja24 <benjaminrromo@outlook.es>
2022-12-11 02:06:37 -0300

    responsive form

--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import DatePicker from 'react-date-picker'
+import Button from 'react-bootstrap/Button';
+
 
 function Formulario() {
     const options = [
@@ -19,41 +21,53 @@ function Formulario() {
     const [value, onChange] = useState(new Date());
     const [valor, setValue] = useState()
     return (
-        <>
-            <br></br>
-            <div className="container-fluid">
-                <form style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-                    <br></br>
-                    <label>Nombre y Apellido:
-                        <input type="text" />
-                    </label>
-                    <br></br>
-                    <label>Rut:
-                        <input type="text" placeholder="11111111-1" />
-                    </label>
-                    <br></br>
-                    <label>Tour:
-                        <select>
+        <div className="container-fluid" >
+            <div className='row' style={{margin:"20px"}}>
+                <center><h1>Reservar</h1></center>
+            </div>
+            <form class="row g-3" style={{display: "flex", flexDirection: "column", alignItems: "center",fontSize:'2vw',magin:'20px'}}>
+                <div class="col-6">
+                    <label class="form-label">Nombre completo:</label>
+                    <input type="text" class="form-control" style={{width:'vw',height:'7vw',fontSize:'1.5vw'}} />
+                </div>
+
+                <div class="col-6">
+                    <label class="form-label">Rut:</label>
+                    <input type="text" class="form-control" placeholder="11111111-1" style={{width:'vw',height:'7vw',fontSize:'1.5vw'}}/>
+                </div>
+
+                <div class="col-6">
+                    <label class="form-label">Tour:</label>
+                    <select class="form-select" style={{width:'vw',height:'7vw',fontSize:'1.5vw'}}>
                             {options.map((option) => (
                                 <option value={option.value}>{option.label}</option>
                             ))}
                         </select>
-                    </label>
-                    <br></br>
-                    <label>Fecha:
-                        <DatePicker onChange={onChange} value={value} />
-                    </label>
-                    <br></br>
-                    <label>E-mail:
-                        <input type="email" />
-                    </label>
-                    <br></br>
-                    <label>Teléfono:
-                        <input type="text" />
-                    </label>
-                </form>
-            </div>
-        </>
+                </div>
+
+                <div class="col-6">
+                    <label class="form-label">Email:</label>
+                    <input type="email" class="form-control" style={{width:'vw',height:'7vw',fontSize:'1.5vw'}}/>
+                </div>
+
+                <div class="col-6">
+                    <label class="form-label">Teléfono:</label>
+                    <input type="text" class="form-control" placeholder="+56912345678" style={{width:'vw',height:'7vw',fontSize:'1.5vw'}}/>
+                </div>
+
+                <div class="col-6">
+                    <label class="form-label">Fecha:</label>
+                    <input type="date" class="form-control" setValue={value}onChange={{onChange}} style={{width:'vw',height:'7vw',fontSize:'1.5vw'}}/>
+                </div>
+                <div class="col-12">
+                    <center><Button variant='outline-success' bg='dark' style={{width:'vw',height:'vw',fontSize:'1.5vw'}}>Reservar</Button></center>
+                </div>        
+            </form>
+
+        </div>
+        
+        
+    
     );
 }
 
